492ff4bde39e32453edf309048a947a758c01e83
Linda Eng <90lineng@gmail.com>
2020-12-09 16:38:11 -0500

    keyboard

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import * as Tone from 'tone';
 function App() {
   let synth = new Tone.PolySynth().toDestination();
   let notes = ['C','D','E','F','G','A','B'];
-
+  let octaves = [1,2]
   function noteDown(note) {
      synth.triggerAttackRelease(note, '16n')
   }
@@ -12,11 +12,21 @@ function App() {
   return (
     <div className="App">
       <div id="container">
+        
         {notes.map((note, id) =>{
+          let hasSharp = true;
+          if(note === 'E' || note === 'B') {
+            hasSharp = false;
+          }
           return(
-            <div key={id} className='whiteNote' data-note={`${note + '4'}`} onMouseDown={ e => noteDown(e.target.getAttribute("data-note"))}>hello</div>
-          )
-        })}
+            <div id ="innerContainer">
+              <div key={id} className='whiteNote' data-note={`${note + '4'}`} onMouseDown={ e => noteDown(e.target.getAttribute("data-note"))}></div>
+              { (hasSharp) ?
+                <div className='blackNote' data-note={`${note + '#4'}`} onMouseDown={e => noteDown(e.target.getAttribute("data-note"))}></div> : null
+              }
+            </div>
+          )}
+        )}
       </div>
     </div>
   );
