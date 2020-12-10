adcab8f942b4f77b3b037477a60dbd8050cac4cc
Linda Eng <90lineng@gmail.com>
2020-12-10 13:47:17 -0500

    keyboard centered

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
 import './App.css';
 import * as Tone from 'tone';
+import React, {useState} from 'react';
 
 function App() {
+  // [activeNotes, noteState] = useState({
+  //   whiteNote: 'whiteNote',
+  //   blackNote: 'blackNote'
+  // })
   let synth = new Tone.PolySynth().toDestination();
   let notes = ['C','D','E','F','G','A','B'];
-  let octaves = [1,2]
-  function noteDown(note) {
-     synth.triggerAttackRelease(note, '16n')
+  let octaves = [0,1];
+
+
+  function noteDown(elem ,note, isSharp) {
+    console.log(elem.target.className)
+    synth.triggerAttackRelease(note, '16n')
+    return true
   }
 
   return (
     <div className="App">
       <div id="container">
-        
-        {notes.map((note, id) =>{
-          let hasSharp = true;
-          if(note === 'E' || note === 'B') {
-            hasSharp = false;
-          }
-          return(
-            <div id ="innerContainer">
-              <div key={id} className='whiteNote' data-note={`${note + '4'}`} onMouseDown={ e => noteDown(e.target.getAttribute("data-note"))}></div>
+      { octaves.map(octave =>{
+        return notes.map((note, id) =>{
+            let hasSharp = true;
+            if(note === 'E' || note === 'B') {
+              hasSharp = false;
+            }
+            return(
+              <div id ="innerContainer">
+              <div key={id} className='whiteNote' data-note={`${note + (octave + 4)}`} onMouseDown={ e => noteDown(e,e.target.getAttribute("data-note"),false)}></div>
               { (hasSharp) ?
-                <div className='blackNote' data-note={`${note + '#4'}`} onMouseDown={e => noteDown(e.target.getAttribute("data-note"))}></div> : null
+                <div className='blackNote' data-note={`${note + '#' + (octave + 4)}`} onMouseDown={e => noteDown(e,e.target.getAttribute("data-note"),true)}></div> : null
               }
-            </div>
-          )}
-        )}
+              </div>
+            )
+        })
+
+        })
+      }
       </div>
     </div>
   );
 }
 
 export default App;
+
