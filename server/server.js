6d26894537fe2811c2c5b964f227b20f01e5a66d
Linda Eng <90lineng@gmail.com>
2021-01-06 14:27:06 -0500

    Express server set-up complete

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.use('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+})
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+});
+
+// static file-serving middleware
+app.use(express.static(path.join(__dirname, '../', 'public')))
+
+// error handling endware
+app.use((err, req, res, next) => {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
+
+
+
+app.listen(process.env.PORT || 8080, ()=>{
+  console.log('poop listening in port 8080')
+});
+
+module.exports = app
